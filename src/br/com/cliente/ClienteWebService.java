1b775ca032a1f728eec911b2ff11241639e2ffe5
vitoraquino <vitoraquino80@gmail.com>
2022-10-18 16:44:22 -0300

    WebService

---
+++ src/br/com/cliente/ClienteWebService.java
@@ -0,0 +1,15 @@
+package br.com.cliente;
+
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+
+public class ClienteWebService {
+
+    public static void main(String[] args) throws IOException {
+        String conteudo = Request.Post("http://localhost:8080/alura-servlets-fundamentos-programacao-web-java/empresas")
+                .addHeader("Accept", "application/json").execute().returnContent().asString();
+
+        System.out.println(conteudo);
+    }
+}
